a17c4aae04c6f9075fcb523504250759beef7625
KrzysztofPempera <k.pempera@gmail.com>
2021-12-02 22:09:06 +0100

    Added register form

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-const login_button = document.getElementById('login');
-const home_button = document.getElementById('home');
+const login_nav_button = document.getElementById('login');
+const home_nav_button = document.getElementById('home');
+const register_nav_button = document.getElementById('register');
 
-login_button.addEventListener("click", function(event) {
+login_nav_button.addEventListener("click", function(event) {
     location.href = 'login.html';
 });
 
-home_button.addEventListener("click", function(event) {
+home_nav_button.addEventListener("click", function(event) {
     location.href = 'index.html';
+});
+
+register_nav_button.addEventListener("click", function(event) {
+    location.href = 'register.html';
 });
 
